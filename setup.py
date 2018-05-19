d136af873fa6c4aabbd7643315b70395a50b595f
s.pyzhov <s.pyzhov@corp.mail.ru>
2018-05-19 21:19:06 +0300

    version 1.0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='babex',
-    version='1.0.1',
+    version='1.0.2',
     install_requires=[
         "pika"
     ],
     description="Имплементация протокола общения между сервисами, через очередь сообщений RabbitMQ.",
     author='Stepan Pyzhov',
+    author_email="turin.tomsk@gmail.com",
     maintainer='Stepan Pyzhov',
     packages=['babex'],
     package_dir={'babex': 'src'},
